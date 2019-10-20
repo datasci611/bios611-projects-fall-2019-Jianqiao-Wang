ec769469723096a72323189367ef73e5544ab183
Jianqiao Wang <jianqiao@live.unc.edu>
2019-10-20 01:15:38 -0400

    project 2

--- project_2/app.R
+++ project_2/app.R
@@ -1,1 +1,107 @@
+library(shiny)
+library(shinydashboard)
+# setwd('~/Documents/GitHub/bios611-projects-fall-2019-Jianqiao-Wang/project_2/')
+source('helper_functions.R')
 
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+  dashboardHeader(title = "Project 2"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Background", tabName = "background", icon = icon("Background")),
+      menuItem("Purpose of Analysis", tabName = "purpose", icon = icon("Purpose of Analysis")),
+      menuItem("Analysis", tabName = "analysis", icon = icon("Purpose of Analysis"),
+               menuSubItem("Question 1-3", tabName = "Q1-3", icon = icon("Question 1-3")),
+               menuSubItem("Question 4", tabName = "Q4", icon = icon("Question 2")))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "background",
+        htmlOutput("background")
+      ), 
+      tabItem(tabName = "purpose",
+        htmlOutput("purpose")
+      ),
+      tabItem(tabName = "Q1-3",
+        fluidRow(
+          sidebarPanel(
+            selectInput('trend_variable',
+                        'Type of Help UMD Provided',
+                        c("Number of People Receiving Food", "Food Pounds", "Clothing Items")
+            )
+          ),
+          mainPanel(
+            plotOutput("trend_plot"),
+            htmlOutput("trend_interpretation")
+          )
+        )
+      ),
+      
+      tabItem(tabName = "Q4",
+          mainPanel(
+            plotOutput("average_food"),
+            htmlOutput("average_food_interpretation")
+          ),
+          sidebarPanel(
+            calculator,
+            selectInput('group',
+                        'Group',
+                        c(1, 2)
+            ),
+            numericInput('number_of_people',
+                         'Number of People',
+                         value=1, min=1, max=60, step=1
+            ),
+            textOutput("food_pounds")
+          )
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  set.seed(435)
+  output$background <- renderUI({
+   HTML(paste(umd_description, data_description, sep="<br/>"))
+  })
+  output$purpose <- renderUI({
+    HTML(paste("<ul><li>", purpose[1], 
+               "</li><li>", purpose[2], 
+               "</li><li>", purpose[3],
+               "</li><li>", purpose[4], "</li><ul>"))
+  })
+  output$trend_plot <- renderPlot({
+    trend(input$trend_variable)
+  })
+  output$trend_interpretation <- renderUI({
+    HTML(paste("<ul><li>", trend_interpretation[input$trend_variable][1,], 
+               "</li><li>", trend_interpretation[input$trend_variable][2,], 
+               "</li><li>", trend_interpretation[input$trend_variable][3,], "</li><ul>"))
+  })
+  
+  output$average_food <- renderPlot({
+    average_food_plot
+  })
+  output$average_food_interpretation <- renderUI({
+    HTML(paste("<ul><li>", average_food_interpretation[1], 
+               "</li><li>", average_food_interpretation[2], 
+               "</li><li>", average_food_interpretation[3],
+               "</li><li>", average_food_interpretation[4],
+               "</li><li>", average_food_interpretation[5],
+               "</li><li>", average_food_interpretation[6],
+               "</li><li>", average_food_interpretation[7],
+               "</li><ul>"))
+  })
+  
+  output$food_pounds <- renderText({
+    if (input$group==1) {
+      paste('Estimated Food Pounds UMD should provide for', input$number_of_people, ' people in Group One:', round(model$one$coefficients * input$number_of_people, 2), 'pounds')
+    } else {
+      paste('Estimated Food Pounds UMD should provide:', input$number_of_people, ' people in Group Two:',round(model$two$coefficients * input$number_of_people, 2), 'pounds')
+    }})
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- project_2/helper_functions.R
+++ project_2/helper_functions.R
@@ -1,1 +1,136 @@
+library(tidyverse)
+library(mclust)
+library(lubridate)
+library(anomalize)
 
+# ---Background---
+umd_description = "Urban Ministries of Durham (UMD) is a program that helps homeless people by providing neighbors with emergency shelter and case management to help them overcome barriers such as unemployment, medical and mental health problems, past criminal convictions and addiction."
+data_description = "The data provided by UMD recorded different kinds of support that UMD provided for homeless people from 1931. It has more than 10 variables including date, family identifiers, financial support, etc."
+
+# ---Purpose of Analysis---
+purpose = c("What is the trend of Number of People Receiving Food Every Day over Time?",
+            "What is the trend of Food Pounds UMD Provided Every Day over Time?",
+            "What is the trend of Clothing Items UMD Provided Every Day over Time?",
+            "How many food pounds does UMD provide for one person? Is there a difference among different families and people?")
+# ---Data Preprocessing---
+# Load UMD data
+# Select Date, Food.Provided.for and Food.Pounds columns.
+umd_df = read.csv('data/UMD_Services_Provided_20190719.tsv', sep = '\t', header = TRUE)
+umd_df = select(umd_df, c(Date, Food.Provided.for, Food.Pounds, Clothing.Items))
+names(umd_df) = c("Date", "Number of People Receiving Food", "Food Pounds", "Clothing Items")
+
+# Formulate Date columns with as.Date
+umd_df$Date = as.Date(umd_df$Date, "%m/%d/%Y")
+
+# Remove rows with missing data
+# Sort data by Date
+umd_df = umd_df %>% 
+  arrange(Date) %>%
+  mutate(year=year(Date)) # extract year from Date variable
+
+# Remove data points after 2020 and data points before 2000
+umd_df = umd_df %>%
+  filter(Date < as.Date('2019-09-24'), Date > as.Date('2000-01-01'))
+
+# IQR method to find outliers
+outliers <- function(x) {
+  lowerq = quantile(x)[2]
+  upperq = quantile(x)[4]
+  iqr = upperq - lowerq
+  upper = (iqr * 1.5) + upperq
+  lower = lowerq - (iqr * 1.5)
+  return(c(upper, lower))
+}
+
+# ---Question 1-3: trend of different variables against time---
+trend <- function(variable){
+  # aggregate value target variable by Date.
+  trend_df = umd_df %>%
+    select(Date, matches(variable)) %>%
+    drop_na() %>%
+    group_by(Date) %>%
+    summarise(total=sum(get(variable)))
+  
+  # find outliers
+  outlier_bound = outliers(trend_df$total)
+  
+  # remove outliers
+  trend_df = trend_df %>%
+    filter(total < outlier_bound[1], total > outlier_bound[2])
+  
+  #
+  p <- ggplot(trend_df, aes(x=Date, y=total)) +
+    geom_point(size=0.2) +
+    geom_smooth() +
+    labs(x='Time',
+         y=paste(variable, 'Every Day'),
+         title=paste(variable, 'Every Day over Time')) +
+    theme(plot.title = element_text(hjust = 0.5))
+  return(p)
+}
+
+trend_interpretation = tibble(`Number of People Receiving Food` = c("Number of People Receiving Food Every Day increases during 2005 and 2017, which is attributable to UMD’s great work.", 
+                                                              "The growth slowed down during 2010 and 2013, which indicates that UMD is ending some people’s homelessness.", 
+                                                              "Number of People Receiving Every Day starts to decrease after 2017, which indicates that UMD has ended many people’s homelessness since 2017."),
+                              `Food Pounds` = c("Food Pounds UMD Provided Every Day generally increase during 2006 and 2017.",
+                                                "Food Pounds UMD Provided Every Day decrease during 2011 and 2013, which indicates that UMD is ending some people’s homelessness.",
+                                                "Food Pounds UMD Provided Every Day decrease after 2017, which indicates that UMD has ended many people’s homelessness since 2017."),
+                              `Clothing Items` = c("Clothing Items UMD Provided Every Day increase during 2002 and 2010.",
+                                                   "Clothing Items UMD Provided Every Day is a convex function of Time during 2011 and 2015.",
+                                                   "Clothing Items UMD Provided Every Day start to decrease after 2015, which indicates that UMD has ended many people’s homelessness since 2015"))
+
+
+
+# ---Question 4: Average Food Pounds per person---
+food_data = umd_df %>%
+  select(`Number of People Receiving Food`, `Food Pounds`) %>%
+  drop_na() %>%
+  filter(`Food Pounds` < 100, `Number of People Receiving Food` < 60)
+
+# Fit EM clustering algorithm
+# Divide data points into 2 clusters by Food.Provided.for and Food.Pounds
+set.seed(435)
+fit = food_data %>%
+  Mclust(G=2)
+
+# Assign each data points with estimated cluster
+food_data$Group = as.factor(fit$classification)
+food_data$uncertainty = fit$uncertainty
+
+# Figure 4: Average Food Pounds per person colored by cluster
+average_food_plot <- ggplot(food_data, aes(x=`Number of People Receiving Food`, y=`Food Pounds`, color=Group, group=Group)) +
+                  geom_point(size=1.5) +
+                  geom_smooth(method='lm', se=FALSE, formula=y~x-1) +
+                  labs(x='Number of People Receiving Food',
+                       y='Food Pounds',
+                       title='Food Pounds against Number of People Receiving Food') +
+                  theme_minimal() +
+                  theme(plot.title = element_text(hjust = 0.5)) +
+                  theme(axis.title=element_text(size=13), title = element_text(size=15))
+# Fit data with simple linear model without intercept by group
+model = list()
+model$one = lm(`Food Pounds`~`Number of People Receiving Food`-1, filter(food_data, Group==1))
+model$two = lm(`Food Pounds`~`Number of People Receiving Food`-1, filter(food_data, Group==2))
+average_food_interpretation = c("There are two groups of data points in this plot: one with big derivative and the other with small derivative.",
+                                "Derivative can be seen as estimated Average Food Pounds per person.",
+                                "EM algorithm appropriately divides the data into two groups as expected.",
+                                "Simple linear model without intercept has a good fit of two groups.",
+                                "For Group 1, UMD provides 8.45 pounds of food for each person.",
+                                "For Group 2, UMD provided 0.62 pounds of food for each person.",
+                                "A big difference in Average Food Pounds per Person exists between these two groups!")
+calculator = "Here is a calculator to estimate pounds of food UMD should provided for certain number of people in different groups:"
+# association <- function(variable_x, variable_y){
+#   association_df = umd_df %>%
+#     select(matches(variable_x), matches(variable_y)) %>%
+#     drop_na()
+#   names(association_df) = c('x', 'y')
+#   outlier_x = outliers(association_df$x)
+#   outlier_y = outliers(association_df$y)
+#   association_df = association_df %>%
+#     filter(x < outlier_x[1], x > outlier_x[2]) %>%
+#     filter(y < outlier_y[1], y > outlier_y[2])
+#   names(association_df) = c(variable_x, variable_y)
+#   p <- ggplot(association_df, aes(x=get(variable_x), y=get(variable_y))) +
+#     geom_point(size=2.5)
+#   return(p)
+# }
